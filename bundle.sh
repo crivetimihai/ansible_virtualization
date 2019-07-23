9488b897746d739ea914a50cb76885107236aa84
Criveti Mihai <crivetimihai@gmail.com>
2019-07-23 04:46:40 -0500

    Added bundle script

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
 # Run lint
 yamllint roles/*
 ansible-lint roles/*
 
-mazer build
+# Release
+my_release=$(mazer build | awk 'END{print $NF}')
 
-cd docs
-mkdocs build
-cd ..
+
+echo "Release ${my_release} to ansible-galaxy?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) mazer publish ${my_release}; break;;
+        No ) exit;;
+    esac
+done
